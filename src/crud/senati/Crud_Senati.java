0b291035c0172c1062f47a99eb1c5fa3ad701b41
Manrique <1601836@senati.pe>
2025-03-31 09:07:43 -0500

    Crear la clase conexion y su metodo conectar

---
+++ src/crud/senati/Crud_Senati.java
@@ -0,0 +1,12 @@
+
+package crud.senati;
+
+public class Crud_Senati {
+    public static void main(String [] args){
+    Conexion cn = new Conexion();
+    
+    cn.conectar();
+    
+    }
+    
+}
